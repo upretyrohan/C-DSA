abfa8e209d7e1e165ddd0b6327abb091b1d5ca8c
Uprety <rohan.uprety@siemens.com>
2021-03-12 15:20:25 +0530

    Circular LL changes

--- linkedlist/CircularLL/delete.cpp
+++ linkedlist/CircularLL/delete.cpp
@@ -12,36 +12,107 @@ struct Node
     }
 };
 
-Node *delHead(Node *head)
-{
-    return NULL;
-}
 
-Node *delAtPos(Node *head, int pos)
+void printCL(Node* head)
 {
-    return NULL;
-}
-void printLL(Node *head)
-{
-    if (head == nullptr)
+    if (head == NULL)
         return;
 
-    Node *curr = head;
-    do
-    {
+    Node* curr = head;
+    do {
         printf("%d ", curr->data);
         curr = curr->next;
-    } while (curr != head);
+    }while (curr != head);
+
     printf("\n");
+}
+
+// O(1) - time complexity
+Node* insertatEnd(Node* head, int data)
+{
+    Node* temp = new Node(data);
+
+    if (head == nullptr)
+    {
+        head = new Node(data);
+        head->next = head;
+        return head;
+    }
+
+    temp->next = head->next;
+    head->next = temp;
+    int tdata = temp->data;
+    temp->data = head->data;
+    head->data = tdata;
+
+    return temp;
+}
+
+
+Node* delHead(Node* head)
+{
+    if (head == NULL)
+        return NULL;
+    
+    Node* curr = head->next;
+
+    int temp = curr->data;
+    curr->data = head->data ;
+    head->data = temp;
+
+    head->next = curr->next;
+    delete curr;
+    return head;
+}
+
+Node* delatPos(Node* head, int pos)
+{
+    if (head == NULL )
+        return nullptr;
+
+    // deleting head
+    if (pos == 1)
+    {
+        Node* curr = head->next;
+        int temp = curr->data;
+        curr->data = head->data;
+        head->data = temp;
+        head->next  = curr->next;
+        delete curr;
+        return head;
+    }
+
+    Node* curr = head;
+    for (int i =0 ; i < pos-2; i++)
+        curr = curr->next;
+    
+    if (curr == NULL)
+        return head;
+
+    Node* temp = curr->next->next;
+    delete curr->next;
+    curr->next = nullptr;
+    curr->next = temp;
+    return head;
 }
 
 int main()
 {
-    Node *head = new Node(10);
-    head->next = new Node(20);
-    head->next->next = new Node(30);
-    head->next->next->next = new Node(40);
-    head->next->next->next->next = head;
-    printLL(head);
-    return 0;
+    Node * head = nullptr;
+   /* head = insertatBegin(head, 10);
+    head = insertatBegin(head, 20);
+    head = insertatBegin(head, 30);
+    head = insertatBegin(head, 40);*/
+    head = insertatEnd(head, 10);
+    head = insertatEnd(head, 20);
+    head = insertatEnd(head, 30);
+    head = insertatEnd(head, 40);
+    head = insertatEnd(head, 50);
+    //delHead(head);
+    head = delatPos(head,1);
+    printCL(head);
+    head = delatPos(head,2);
+    printCL(head);
+    head = delatPos(head,3);
+    printCL(head);
 }
--- linkedlist/CircularLL/insert.cpp
+++ linkedlist/CircularLL/insert.cpp
@@ -12,8 +12,24 @@ struct Node
     }
 };
 
+
+void printCL(Node* head)
+{
+    if (head == NULL)
+        return;
+
+    Node* curr = head;
+    do {
+        printf("%d ", curr->data);
+        curr = curr->next;
+    }while (curr != head);
+}
+
+// O(1) - time complexity
 Node* insertatBegin(Node *head, int data)
 {
+    Node* temp = new Node(data);
+
     if (head == nullptr)
     {
         head = new Node(data);
@@ -21,19 +37,47 @@ Node* insertatBegin(Node *head, int data)
         return head;
     }
 
-    Node* curr = head;
+    temp->next = head->next;
+    head->next = temp;
+    int tdata = temp->data;
+    temp->data = head->data;
+    head->data = tdata;
+
+    return head;
+}
+
+// O(1) - time complexity
+Node* insertatEnd(Node* head, int data)
+{
     Node* temp = new Node(data);
-    int tempdata = curr->data;
-    curr->data = temp->data;
-    temp->next = curr;
-    temp->data = tempdata;
+
+    if (head == nullptr)
+    {
+        head = new Node(data);
+        head->next = head;
+        return head;
+    }
+
+    temp->next = head->next;
+    head->next = temp;
+    int tdata = temp->data;
+    temp->data = head->data;
+    head->data = tdata;
+
+    return temp;
 }
 
 int main()
 {
     Node * head = nullptr;
-    head = insertatBegin(head, 10);
+   /* head = insertatBegin(head, 10);
     head = insertatBegin(head, 20);
     head = insertatBegin(head, 30);
-    head = insertatBegin(head, 40);
+    head = insertatBegin(head, 40);*/
+    head = insertatEnd(head, 10);
+    head = insertatEnd(head, 20);
+    head = insertatEnd(head, 30);
+    head = insertatEnd(head, 40);
+    head = insertatEnd(head, 50);
+    printCL(head);
 }
